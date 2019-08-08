4fe87112efd8e833f47dcf0aa2e196c8cd40bf67
“mackwang” <641212003@qq.com>
2019-08-08 10:51:21 +0800

    fix: 修改初始化数据

--- data.sql
+++ data.sql
@@ -1,25 +1,25 @@
-insert into user (id, password, name, logo, account, mobile, needChangePassword, userCode, nickname, lastPasswordResetDate)
+insert into user (id, password, name, logo, account, mobile, needChangePassword, userCode, lastPasswordResetDate, createdAt, updatedAt)
 values
-  (1, '$2b$10$bt.jm3Opex8oC9r/B/F9ruvg2KdzerOH07QMhBnAOOwHPSg.7yLny', 'tom', 'http://www.yurencloud.com/_nuxt/img/avatar.902cc14.png', 'mackwang', '15757130092', 0, 'USER3303003030123', now(), now(), now()),
-  (2, '$2b$10$bt.jm3Opex8oC9r/B/F9ruvg2KdzerOH07QMhBnAOOwHPSg.7yLny', 'bob', 'http://www.yurencloud.com/_nuxt/img/avatar.902cc14.png', 'mackbob', '15757130093', 1, 'USER3303003030124', now(), now(), now());
+(1, '$2b$10$bt.jm3Opex8oC9r/B/F9ruvg2KdzerOH07QMhBnAOOwHPSg.7yLny', 'tom', 'http://www.yurencloud.com/_nuxt/img/avatar.902cc14.png', 'mackwang', '15757130092', 0, 'USER3303003030123', now(), now(), now()),
+(2, '$2b$10$bt.jm3Opex8oC9r/B/F9ruvg2KdzerOH07QMhBnAOOwHPSg.7yLny', 'bob', 'http://www.yurencloud.com/_nuxt/img/avatar.902cc14.png', 'mackbob', '15757130093', 1, 'USER3303003030124', now(), now(), now());
 
 
 insert into role (id, name)
 values
-  (1, 'ROLE_ADMIN'),
-  (2, 'ROLE_USER');
+(1, 'ROLE_ADMIN'),
+(2, 'ROLE_USER');
 
-insert into user_role (id, user_id, role_id)
+insert into user_role (id, userId, roleId)
 values
-  (1, 1, 1),
-  (2, 2, 2);
+(1, 1, 1),
+(2, 2, 2);
 
 insert into merchant (id, code, merchantAppId, name, status, type)
 values
-  (1, 'SGSJ000145', 'APPL2019070811444822900000400000)','小王1',1,2);
+(1, 'SGSJ000145', 'APPL2019070811444822900000400000)','小王1',1,2);
 
 insert into permission (id, code, uri)
 values
-  (1, 'SGSJ000145', 'home.index'),
-  (2, 'SGSJ000145', 'business'),
-  (3, 'SGSJ000145', 'business.order');
+(1, 'SGSJ000145', 'home.index'),
+(2, 'SGSJ000145', 'business'),
+(3, 'SGSJ000145', 'business.order');
